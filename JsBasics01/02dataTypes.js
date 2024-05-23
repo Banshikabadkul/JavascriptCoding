5640203b759a1ab8bf655575584233cc034f805d
Banshika <banshikabadkul@gmail.com>
2024-05-23 16:05:21 +0530

    dataType02 is updated

---
+++ JsBasics01/02dataTypes.js
@@ -0,0 +1,10 @@
+"use strict" // allow js to use newer version for all codes
+
+console.log(3+3)  
+
+let name="Banshika"
+let age =  18
+let isLoggedIn = false
+
+console.log(typeof(undefined)); // data type is undefined
+console.log(typeof(null)); // data type is object
